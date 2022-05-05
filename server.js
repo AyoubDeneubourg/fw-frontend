840a37c677730eb23cb6597a69602883e766b457
Ayoub Deneubourg <ayoub.deneubourg@student.ehb.be>
2022-05-05 21:30:53 +0200

    change path

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require("path");
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static("./dist/project5"));
+app.use(express.static("./dist/final-work"));
 
 app.get("/*", (req, res) =>
-  res.sendFile("index.html", { root: "dist/project5/" })
+  res.sendFile("index.html", { root: "dist/final-work/" })
 );
 
 // Start the app by listening on the default Heroku port
