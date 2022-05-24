a483c45b0ffbec29c766eed068b936d9d05c97e2
Ayoub Deneubourg <ayoub.deneubourg@student.ehb.be>
2022-05-24 21:39:23 +0200

    heroku

---
+++ src/app/core/services/stripe/stripe.service.ts
@@ -0,0 +1,52 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { loadStripe } from '@stripe/stripe-js';
+import { environment } from 'src/environments/environment';
+import { getHeaders } from '../authorization/authorization';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StripeService {
+
+  readonly apiUrl = "https://fw-api-gateway.herokuapp.com";
+
+  stripePromise = loadStripe(environment.stripe);
+
+
+  constructor(private http: HttpClient) { }
+
+
+   // here we create a payment object
+   public payment = {
+    name: 'Iphone',
+    currency: 'usd',
+    amount: 99900,
+    quantity: '1',
+    cancelUrl: 'http://localhost:4200/cancel',
+    successUrl: 'http://localhost:4200/success',
+  };
+
+
+
+  
+  public async pay() {
+    
+    const stripe = await this.stripePromise;
+
+    this.http
+    .post(`${this.apiUrl}/api/payment`, this.payment, getHeaders())
+    .subscribe((data: any) => {
+      // I use stripe to redirect To Checkout page of Stripe platform
+      console.log(data);
+      alert(data.id);
+      stripe.redirectToCheckout({
+        sessionId: data.id,
+      });
+
+    });
+
+  }
+
+
+}
